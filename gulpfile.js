1538685be8c39a1ac62487002ffee9c7bae93f71
mikeybkats <michaelbkats@gmail.com>
2018-09-21 15:51:06 -0700

    updating gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,6 @@ gulp.task("default", () =>
     .pipe(gulp.dest("dist"))
 )
 
-gulp.task("tests", () =>
-  gulp
-    .src(["test/*.js"])
-    .pipe(
-      babel({
-        presets: ["env"]
-      })
-    )
-    .pipe(gulp.dest("dist/test"))
-)
-
 gulp.task("watch", () => {
-  gulp.watch("test/*.js", ["tests"])
   gulp.watch("src/*.js", ["default"])
 })
