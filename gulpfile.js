43fb27e93136f6c4d5591d1813ef43a4a589b0b0
mikeybkats <michaelbkats@gmail.com>
2018-09-21 13:35:58 -0700

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ const gulp = require("gulp")
 const babel = require("gulp-babel")
 
 gulp.task("default", () =>
-    gulp.src([
-        "src/app.js"
-    ])
-        .pipe(babel({
-            presets: ["env"]
-        }))
-        .pipe(gulp.dest("dist"))
+  gulp
+    .src(["src/*.js", "src/app.js"])
+    .pipe(
+      babel({
+        presets: ["env"]
+      })
+    )
+    .pipe(gulp.dest("dist"))
 )
