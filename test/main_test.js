76c3c928f3cc009e46d027a6e5a1fe65c3d1b59c
mikeybkats <michaelbkats@gmail.com>
2018-09-21 15:57:15 -0700

    adding sanity check test

---
+++ test/main_test.js
@@ -0,0 +1,11 @@
+const expect = require("chai").expect
+
+// Test Suite
+describe("Mocha", () => {
+  // Test spec (unit test)
+  it("should run our tests using npm", () => {
+    // contains all the expectations
+    // ok is an assertion that tests for truthy
+    expect(true).to.be.ok
+  })
+})
